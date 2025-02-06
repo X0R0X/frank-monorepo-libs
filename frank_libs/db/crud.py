b6ef2aa9fc28d14caf803f88c706e7f075b2d22c
Jakub Schimer <jakub.schimer@protonmail.com>
2025-02-06 22:36:53 +0100

    Add postgres crud/models

---
+++ frank_libs/db/crud.py
@@ -0,0 +1,213 @@
+from enum import Enum
+
+from ez_lib.postgres import PgSessionSingleton, mapping_result_to_list
+from ez_lib.types import json_types
+from sqlalchemy import select, update
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql.functions import now
+
+from frank_libs.db.models import (
+    UserModel,
+    DialogueTreeModel,
+    CompanyModel,
+    SlackUserModel
+)
+
+
+class UserRole(Enum):
+    # Can view dialogue trees but not create / edit them, and can not publish
+    # them to Slack users
+    VIEWER = "viewer"
+    # Can create / edit / publish dialogue trees but can not add new users
+    PUBLISHER = "publisher"
+    # Can do everything as publisher, and also can create new users (via email
+    # invite links)
+    ADMIN = "admin"
+
+
+async def mk_user(
+        session: AsyncSession,
+        first_name: str,
+        second_name: str,
+        email: str,
+        role: UserRole,
+        passwd_hash: str
+) -> UserModel:
+    user = UserModel(
+        first_name=first_name,
+        second_name=second_name,
+        email=email,
+        passwd_hash=passwd_hash,
+        role=role.value,
+    )
+    session.add(user)
+    await session.commit()
+
+    return user
+
+
+async def fetch_user(session: AsyncSession, email: str) -> UserModel:
+    stmt = select(UserModel).where(UserModel.email == email)
+    return await session.scalar(stmt)
+
+
+async def update_user_last_login(session: AsyncSession, id_: int):
+    stmt = update(UserModel).where(UserModel.id == id_).values(last_login=now())
+    await session.execute(stmt)
+    await session.commit()
+
+
+async def save_dialogue_tree(
+        session: AsyncSession,
+        user_id: int,
+        table_id: int | None,
+        title: str,
+        tree_json: dict
+) -> DialogueTreeModel | None:
+    if table_id is not None:
+        stmt = update(DialogueTreeModel).where(
+            DialogueTreeModel.id == table_id
+        ).values(
+            data=tree_json,
+            title=title,
+            last_update_at=now(),
+            update_count=DialogueTreeModel.update_count + 1,
+            published=False
+        )
+        # todo return TreeModel as well ?
+        await session.execute(stmt)
+        await session.commit()
+    else:
+        tree = DialogueTreeModel(
+            publisher_id=user_id,
+            title=title,
+            published=False,
+            data=tree_json,
+            created_at=now(),
+            last_update_at=now(),
+            update_count=0
+        )
+        session.add(tree)
+        await session.commit()
+
+        return tree
+
+
+async def fetch_user_trees(session: AsyncSession, user_id: int):
+    stmt = select(DialogueTreeModel).where(
+        DialogueTreeModel.publisher_id == user_id
+    ).where(DialogueTreeModel.deleted_at == None)
+    result = await session.execute(stmt)
+    return result.mappings().all()
+
+
+async def fetch_user_tree(
+        session: AsyncSession, tree_id: int
+) -> DialogueTreeModel | None:
+    stmt = select(DialogueTreeModel).where(DialogueTreeModel.id == tree_id)
+    return await session.scalar(stmt)
+
+
+async def set_user_tree_published(session: AsyncSession, tree_id: int):
+    stmt = update(DialogueTreeModel).where(
+        DialogueTreeModel.id == tree_id
+    ).values(
+        published=True
+    )
+    await session.execute(stmt)
+    await session.commit()
+
+
+async def set_user_tree_deleted(session: AsyncSession, tree_id: int) -> bool:
+    stmt = update(DialogueTreeModel).where(
+        DialogueTreeModel.id == tree_id
+    ).values(deleted_at=now())
+
+    ex = await session.execute(stmt)
+    await session.commit()
+
+    return ex.rowcount == 1
+
+
+async def fetch_companies() -> list[CompanyModel]:
+    async with PgSessionSingleton.get_session() as session:
+        stmt = select(CompanyModel).where(CompanyModel.is_active == True)
+        result = await session.execute(stmt)
+
+        return mapping_result_to_list(result, 'CompanyModel')
+
+
+async def create_company_users(
+        company_id: int,
+        cache_timestamp: int,
+        users_dict: dict
+) -> list[SlackUserModel]:
+    async with PgSessionSingleton.get_session() as session:
+        slack_users = []
+        for user_def in users_dict:
+            if not user_def['is_bot'] and not user_def['deleted']:
+                slack_user = SlackUserModel(company_id=company_id)
+                slack_user.from_dict(user_def)
+                slack_users.append(slack_user)
+
+        stmt = update(CompanyModel).where(CompanyModel.id == company_id).values(
+            last_update=cache_timestamp
+        )
+        await session.execute(stmt)
+        session.add_all(slack_users)
+        await session.commit()
+
+        return slack_users
+
+
+async def fetch_company_users(company_id: int) -> list[SlackUserModel]:
+    async with PgSessionSingleton.get_session() as session:
+        stmt = select(SlackUserModel).where(
+            SlackUserModel.company_id == company_id)
+        result = await session.execute(stmt)
+
+        # noinspection PyTypeChecker
+        return mapping_result_to_list(result, 'SlackUserModel')
+
+
+async def update_company_users(
+        company_id: int,
+        cache_timestamp: int,
+        current_users: dict[str, SlackUserModel],
+        users_dict: list[dict[str, json_types]],
+) -> (list[SlackUserModel], list[SlackUserModel]):
+    async with PgSessionSingleton.get_session() as session:
+        updated_users = []
+        new_users = []
+        for user_def in users_dict:
+            if not user_def['is_bot'] and not user_def['deleted']:
+                if user_def['id'] not in current_users.keys():
+                    slack_user = SlackUserModel(company_id=company_id)
+                    slack_user.from_dict(user_def)
+                    new_users.append(slack_user)
+                else:
+                    slack_user = SlackUserModel(date_updated=now())
+                    slack_user.from_dict(user_def)
+                    updated_users.append(slack_user)
+
+                stmt = insert(SlackUserModel).values(
+                    slack_user.to_values_dict(("company_id",))
+                )
+
+                do_update_stmt = stmt.on_conflict_do_update(
+                    index_elements=['slack_id'],
+                    set_=slack_user.to_values_dict(("date_updated",)),
+                )
+                await session.execute(do_update_stmt)
+
+        # Update Company.last_update = now()
+        await session.execute(
+            update(CompanyModel).where(
+                CompanyModel.id == company_id
+            ).values(users_cache_ts=cache_timestamp)
+        )
+
+        await session.commit()
+
+        return new_users, updated_users
---
+++ frank_libs/db/models.py
@@ -0,0 +1,127 @@
+from datetime import datetime
+from typing import Any, NewType
+
+from ez_lib.postgres import AbstractModelHelper
+from ez_lib.types import json_ser
+from sqlalchemy import JSON, String, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.sql.functions import now
+
+SlackIdStr_9 = NewType("SlackIdStr_9", str)
+SlackStr_20 = NewType("SlackStr_20", str)
+SlackStr_80 = NewType("SlackStr_80", str)
+SlackStr_512 = NewType("SlackStr_512", str)
+DbStr_64 = NewType("SlackStr_64", str)
+
+
+class AbstractModel(AbstractModelHelper, DeclarativeBase):
+    type_annotation_map = {
+        json_ser: JSON,
+        SlackIdStr_9: String(9),
+        SlackStr_20: String(20),
+        SlackStr_80: String(80),
+        SlackStr_512: String(512),
+        DbStr_64: String(64)
+    }
+
+
+class UserModel(AbstractModel):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str]
+    second_name: Mapped[str]
+    email: Mapped[str]
+    passwd_hash: Mapped[str]
+    role: Mapped[str]
+    date_created: Mapped[datetime] = mapped_column(default=now())
+    last_login: Mapped[datetime | None]
+
+    def __str__(self):
+        return (
+            f"id={self.id}, first_name={self.first_name}, "
+            f"last_name={self.last_login}, email={self.email}, "
+            f"passwd_hash={self.passwd_hash}, role={self.role}, "
+            f"date_created={self.date_created}, last_login={self.last_login}"
+        )
+
+
+class DialogueTreeModel(AbstractModel):
+    __tablename__ = 'dialogue_trees'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    publisher_id: Mapped[int]
+    title: Mapped[str]
+    published: Mapped[bool]
+    data: Mapped[json_ser]
+    # data: Mapped[dict[str, Any]]
+    created_at: Mapped[datetime]
+    last_update_at: Mapped[datetime]
+    update_count: Mapped[int]
+    deleted_at: Mapped[datetime | None]
+
+
+class SlackUserModel(AbstractModel):
+    def __init__(self, **kw: Any):
+        super().__init__(**kw)
+
+    __tablename__ = "slack_users"
+
+    _serialize_map = {
+        "slack_id": "id"
+    }
+
+    _except_fields = [
+        "id",
+        "date_created",
+        'company_id',
+        "date_updated"
+    ]
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    company_id: Mapped[int] = mapped_column(ForeignKey('company.id'))
+    date_created: Mapped[datetime] = mapped_column(default=now())
+    date_updated: Mapped[datetime]
+    deleted: Mapped[bool] = mapped_column(default=False)
+
+    slack_id: Mapped[SlackStr_80]
+    team_id: Mapped[SlackIdStr_9]
+    name: Mapped[SlackStr_80]
+    color: Mapped[SlackStr_80]
+    real_name: Mapped[SlackStr_80]
+    tz: Mapped[SlackStr_80]
+    tz_label: Mapped[SlackStr_80]
+    tz_offset: Mapped[int]
+
+    profile__first_name: Mapped[SlackStr_80]
+    profile__last_name: Mapped[SlackStr_80]
+    profile__title: Mapped[SlackStr_80]
+    profile__phone: Mapped[SlackStr_80]
+    profile__skype: Mapped[SlackStr_80]
+    profile__real_name: Mapped[SlackStr_80]
+    profile__display_name: Mapped[SlackStr_80]
+    profile__avatar_hash: Mapped[SlackStr_80]
+    profile__image_original: Mapped[SlackStr_80]
+
+    is_admin: Mapped[bool]
+    is_owner: Mapped[bool]
+    is_primary_owner: Mapped[bool]
+    is_restricted: Mapped[bool]
+    is_ultra_restricted: Mapped[bool]
+    is_app_user: Mapped[bool]
+    updated: Mapped[int]
+    is_email_confirmed: Mapped[bool]
+    who_can_share_contact_card: Mapped[SlackStr_80]
+
+
+class CompanyModel(AbstractModel):
+    __tablename__ = 'company'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[DbStr_64]
+    http_port: Mapped[int]
+    slack_token: Mapped[DbStr_64]
+    slack_secret: Mapped[DbStr_64]
+    users_update_ts: Mapped[int]
+    is_active: Mapped[bool]
+    users_cache_ts: Mapped[int]
